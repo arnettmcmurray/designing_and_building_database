2de0d41d4fb8609fb37eb9eb5e4c79e43e417f6c
Arnett McMurray <arnettmcmurray@Arnetts-Mac-mini.local>
2025-08-05 14:45:40 -0500

    Add full drop_all_tables.sql script

--- drop_all_tables.sql
+++ drop_all_tables.sql
@@ -1,1 +1,10 @@
--- Placeholder content for drop_all_tables.sql
+PRAGMA foreign_keys = OFF;
+
+DROP TABLE IF EXISTS service_history;
+DROP TABLE IF EXISTS invoices;
+DROP TABLE IF EXISTS cars;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS salespeople;
+DROP TABLE IF EXISTS mechanics;
+
+PRAGMA foreign_keys = ON;
